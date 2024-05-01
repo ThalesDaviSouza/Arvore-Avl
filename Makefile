78615e258b17c282bf29e080687109b5e4b5b01f
Thales Davi de Souza <thalesdavisoul@gmail.com>
2024-05-01 18:02:43 -0300

    Starting to implement unit tests to insertion in avl tree

--- Makefile
+++ Makefile
@@ -8,5 +8,12 @@ main.o: main.c
 avl_tree.o: ./src/avl_tree.c ./include/avl_tree.h
 	gcc -c .\src\avl_tree.c
 
+test-insertion-avl-tree: ./tests/insertion_avl_tree.c 
+	@echo "Starting insertion tests..."
+	gcc ./tests/insertion_avl_tree.c -o insertion_avl_tree_test
+	insertion_avl_tree_test
+	@echo "Tests done"
+	make clean
+
 clean:
 	del *.o *.exe
---
+++ tests/insertion_avl_tree.c
@@ -0,0 +1,89 @@
+#include "../libs/minunit.h"
+#include "../src/avl_tree.c"
+#include <stdbool.h>
+
+bool treesEqual(node* h1, node* h2){
+    if(h1 == NULL && h2 == NULL) return true;
+    else if(h1 == NULL) return false;
+    else if(h2 == NULL) return false;
+    
+    if(h1->value == h2->value){
+        bool left = treesEqual(h1->left, h2->left);
+        bool right = treesEqual(h1->right, h2->right);
+
+        return left && right;
+    }
+    else{
+        return false;
+    }
+}
+
+
+MU_TEST(testeCase){
+    
+
+    //mu_assert(result == TRUE, "In array base test 2, the expected result is different from the code's output.");
+}
+
+MU_TEST(SimpleRR){
+    node* head1 = addNode(1, NULL);
+    head1 = addNode(2, head1);
+    head1 = addNode(3, head1);
+    head1 = addNode(4, head1);
+    
+    node* head2 = createNode(2);
+    head2->left = createNode(1);
+    head2->right = createNode(3);
+    head2->right->right = createNode(4);    
+
+
+    mu_assert(treesEqual(head1, head2) == true, "In simple RR test 1, the expected result is different from the code's output.");
+}
+
+MU_TEST(SimpleRR2){
+    node* head1 = addNode(10, NULL);
+    head1 = addNode(20, head1);
+    head1 = addNode(30, head1);
+    head1 = addNode(40, head1);
+    head1 = addNode(22, head1);
+    head1 = addNode(43, head1);
+    
+    node* head2 = createNode(30);
+    head2->left = createNode(20);
+    head2->left->left = createNode(10);
+    head2->left->right = createNode(22);
+    head2->right = createNode(40);
+    head2->right->right = createNode(43);
+
+    mu_assert(treesEqual(head1, head2) == true, "In simple RR test 2, the expected result is different from the code's output.");
+}
+
+MU_TEST(SimpleLL){
+    node* head1 = addNode(10, NULL);
+    head1 = addNode(-20, head1);
+    head1 = addNode(-30, head1);
+    head1 = addNode(-40, head1);
+    
+    node* head2 = createNode(-20);
+    head2->left = createNode(-30);
+    head2->left->left = createNode(-40);
+    head2->right = createNode(10);
+
+    mu_assert(treesEqual(head1, head2) == true, "In simple LL test 1, the expected result is different from the code's output.");
+}
+
+
+MU_TEST_SUITE(test_suite){
+    MU_RUN_TEST(SimpleRR);
+    MU_RUN_TEST(SimpleRR2);
+    MU_RUN_TEST(SimpleLL);
+    
+}
+
+
+int main(int argc, char** argv){
+    MU_RUN_SUITE(test_suite);
+    MU_REPORT();
+
+    return MU_EXIT_CODE;
+}
